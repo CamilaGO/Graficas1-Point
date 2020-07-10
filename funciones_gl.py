8ef1fe838959802f849ad5902b334910adb2c7f9
CamilaGO <gon18398@uvg.edu.gt>
2020-07-10 17:30:20 -0500

    ready

---
+++ funciones_gl.py
@@ -0,0 +1,99 @@
+"""
+Paula Camila Gonzalez Ortega
+18398
+"""
+import struct
+
+def char(c):
+	return struct.pack('=c', c.encode('ascii'))
+
+def word(c):
+	return struct.pack('=h', c)
+
+def dword(c):
+	return struct.pack('=l', c)
+
+def changecolor(r, g, b):
+	return bytes([b, g, r])
+
+BLACK = changecolor(0,0,0)
+WHITE = changecolor(255,255,255)
+RED = changecolor(255, 0, 0)
+
+class Render(object):
+	def __init__(self, width, height, r, g, b):
+		self.framebuffer = []
+		self.clear_color = BLACK
+		self.curr_color = RED
+		self.glCreateWindow(width, height)
+		self.glClearColor(r, g, b)
+		self.glclear()
+
+	def glInit():
+		#Se inicializan variables
+		pass
+
+	def glCreateWindow(self, width, height):
+		self.width = width
+		self.height = height
+
+	def glViewPort(self, x, y, width, height):
+		self.vpWidth = width
+		self.vpHeight = height
+		self.vpx = x
+		self.vpy = y
+
+	def glclear(self):
+		self.framebuffer = [
+		[self.clear_color for x in range(self.width)]
+		for y in range(self.height)
+		]
+
+	def glClearColor(self, r, g, b):
+		red = round(r*255)
+		green = round(g*255)
+		blue = round(g*255)
+		self.clear_color = changecolor(red, green, blue)
+
+	def glVertex(self, x, y):
+		new_x = round((x+1)*(self.vpWidth/2)+self.vpx)
+		new_y = round((y+1)*(self.vpHeight/2)+self.vpy)
+		self.framebuffer[new_y][new_x] = self.curr_color
+	
+	def glColor(self, r=1, g=1, b=1):
+		red = round(r*255)
+		green = round(g*255)
+		blue = round(g*255)
+		self.curr_color = changecolor(red, green, blue)
+
+	def finish(self, filename):
+		f = open(filename, 'bw')
+
+		# file header
+		f.write(char('B'))
+		f.write(char('M'))
+		f.write(dword(14 + 40 + self.width * self.height * 3))
+		f.write(dword(0))
+		f.write(dword(14 + 40))
+
+		# image header
+		f.write(dword(40))
+		f.write(dword(self.width))
+		f.write(dword(self.height))
+		f.write(word(1))
+		f.write(word(24))
+		f.write(dword(0))
+		f.write(dword(self.width * self.height * 3))
+		f.write(dword(0))
+		f.write(dword(0))
+		f.write(dword(0))
+		f.write(dword(0))
+
+		# pixel data
+		for x in range(self.width):
+			for y in range(self.height):
+				f.write(self.framebuffer[y][x])
+
+
+		f.close()
+
---
+++ gl.py
@@ -0,0 +1,20 @@
+from funciones_gl import Render, changecolor
+
+posX = 250
+posY = 250
+width = 1000
+height = 1000
+
+bitmap = Render(width, height, 0, 0, 0) #los ultimos tres son los colores son los del background
+
+bitmap.glViewPort(posX, posY, width - 500 , height - 500)
+
+bitmap.glColor(1, 0, 0) #estos colores son los que se usaran en Vertex
+
+bitmap.glVertex(0, 0)
+
+bitmap.finish('out.bmp')
+
+
+
+
