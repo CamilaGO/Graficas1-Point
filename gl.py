0bb9f08e367c40a23281a55987ef1c181584dbac
CamilaGO <gon18398@uvg.edu.gt>
2020-07-21 16:28:27 -0500

    mejoras en el render

--- gl.py
+++ gl.py
@@ -9,9 +9,12 @@ posY = 250
 width = 1000
 height = 1000
 
-bitmap = Render(width, height, 0, 0, 0) #los ultimos tres son los colores son los del background
+bitmap = Render(width, height) #los ultimos tres son los colores son los del background
 
 bitmap.glViewPort(posX, posY, width - 500 , height - 500)
+
+bitmap.glClearColor(0, 0, 0)
+bitmap.glclear()
 
 bitmap.glColor(1, 0, 0) #estos colores son los que se usaran en Vertex
 
